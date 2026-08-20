21ae9d3cf797d889c3d07076341b8f5d7521f066
ba-reynolds <166666791+ba-reynolds@users.noreply.github.com>
2026-08-19 22:41:53 -0300

    nickname-default: hide unset birthdays (0001-01-01 sentinel); drop legacy backfill migration

--- server/cmd/migrate/migrations/000022_backfill-display-name.down.sql
+++
@@ -1,7 +0,0 @@
--- Reverse the display_name backfill. Lossy: a user who manually set their
--- display name equal to their username would also be reset to ''.
-UPDATE user_profiles up
-SET display_name = ''
-FROM users u
-WHERE up.user_id = u.user_id
-  AND up.display_name = u.username;
